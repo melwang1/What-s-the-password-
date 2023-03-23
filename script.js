1bf1845cb0b16b276c6c3afba818a821ef2563dd
Melanie Wang <melaniiewang@hotmail.com>
2023-03-23 18:06:07 -0400

    added password requirements and variables

--- script.js
+++ script.js
@@ -10,5 +10,15 @@ function writePassword() {
 
 }
 
+function generatePassword(){
+  var passwordlength = parseInt(prompt("Enter Password Length"))
+  var username = prompt("Enter Username")
+  if(passwordlength< 8 || passwordlength>128 || isNaN(passwordlength)){
+    return "Please enter password length between 8-128"
+  }
+  console.log(passwordlength,username)
+}
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
